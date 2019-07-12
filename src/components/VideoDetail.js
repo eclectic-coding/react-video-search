f3d3834331af673609fbd53974c4eea6141373b0
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-07-12 16:08:55 -0400

    Debug console errors

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,18 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading ... </div>
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe
+          src={videoSrc}
+          title={video.snippet.title}
+          alt={video.snippet.title}
+        />
+      </div>
       <div className="ui segment">
         <h4>{video.snippet.title}</h4>
         <p>{video.snippet.desciption}</p>
